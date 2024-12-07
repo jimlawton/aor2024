1b465d8196b783a9131b236ea4c363c130975e63
Jim Lawton <jim.lawton@gmail.com>
2024-12-07 12:42:45 +0000

    Add day 7 solution.

---
+++ day_07/solution.rs
@@ -0,0 +1,26 @@
+ipub struct LogQuery<'a> {
+    logs: &'a Vec<String>,
+}
+
+// 1. Finish the implementation of LogQuery
+impl<'a> LogQuery<'a> {
+    // 2. Create a public associated function named `new()` that will take a reference to a vector of strings
+    pub fn new(logs: &'a Vec<String>) -> LogQuery {
+        Self {
+            logs
+        }
+    }
+
+    // 3. Create a public method named `search` that accepts a string slice and finds it from the logs and
+    //    returns a vector of references to those logs.
+    pub fn search(&self, keyword: &str) -> Vec<&'a String> {
+        let mut results = Vec::new();
+        for record in self.logs.iter() {
+            if record.contains(keyword) {
+                results.push(record);
+            }
+        }
+        results
+    }
+}
+
